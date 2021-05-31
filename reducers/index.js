bdee89a55285a6a0b7f8d01ab35b1a069232097f
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-05-30 21:53:39 -0300

    Minor fixes

--- reducers/index.js
+++ reducers/index.js
@@ -1,12 +1,13 @@
 import sessionReducer from "../session/session.reducer";
 import {combineReducers} from "redux";
+import {LOGOUT} from "../session/session.actions";
 
 const appReducer = combineReducers({
     session: sessionReducer,
 })
 
 const rootReducer = (state, action) => {
-    if (action.type === 'USER_LOGOUT') {
+    if (action.type === LOGOUT) {
         return appReducer(undefined, action)
     }
 
