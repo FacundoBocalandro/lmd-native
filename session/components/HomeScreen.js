2d8642e196ffc5da96427bdeb5a9493a0437945f
nicolefox <fox_nicole@hotmail.com>
2021-06-13 11:22:14 -0300

    home container position fixed

--- session/components/HomeScreen.js
+++ session/components/HomeScreen.js
@@ -44,10 +44,6 @@ const HomeScreen = ({logout}) => {
 const styles = StyleSheet.create({
     container: {
         position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
         width: '100%',
         height: '100%',
         flex: 1,
